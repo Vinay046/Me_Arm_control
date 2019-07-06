28b5c614bce6ac8ef914c0c7c9bc9b92e9ad216f
Vinay Patil <vinayvp.98@gmail.com>
2019-07-06 22:01:25 +0530

    Changes in the UI
    modified the size of the window

--- GUI/GUI_Servo_control.py
+++ GUI/GUI_Servo_control.py
@@ -1,25 +1,30 @@
-from tkinter import *
-
+import tkinter as tk
 def show_values():
     print (w1.get(), w2.get(), w3.get())
 def grab():
 	print("Grabbed the object")
-master = Tk()
+
+master = tk.Tk()
+
+master.geometry("700x300")
+master.resizable(0,0)
 master.title("Servo Angle Control")
-w1 = Scale(master, from_=0, to=180,length = 600,tickinterval=10, orient=HORIZONTAL)
+w1 = tk.Scale(master, from_=0, to=180,length = 600,tickinterval=180, orient=tk.HORIZONTAL)
 w1.set(0)
 w1.pack()
-w2 = Scale(master, from_=0, to=180,length = 600,tickinterval=10, orient=HORIZONTAL)
+w2 = tk.Scale(master, from_=0, to=180,length = 600,tickinterval=180, orient=tk.HORIZONTAL)
 w2.set(0)
 w2.pack()
-w3 = Scale(master, from_=0, to=180,length = 600,tickinterval=10, orient=HORIZONTAL)
+w3 = tk.Scale(master, from_=0, to=180,length = 600,tickinterval=180, orient=tk.HORIZONTAL)
 w3.set(0)
 w3.pack()
-label1 = Label(master,text="")
+label1 = tk.Label(master,text="")
 label1.pack()
-Button(master, text='Show', command=show_values).pack()
-label2 = Label(master,text="")
+tk.Button(master, text='Show', command=show_values).pack()
+label2 = tk.Label(master,text="")
 label2.pack()
-Button(master, text='GRAB', command=grab).pack()
+tk.Button(master, text='GRAB', command=grab).pack()
+label3 = tk.Label(master,text="")
+label3.pack()
 
-mainloop()
+master.mainloop()
