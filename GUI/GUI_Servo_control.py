9bd85b2297b00845fad04391de954e1a4325ef56
Vinay Patil <vinayvp.98@gmail.com>
2019-07-06 20:16:09 +0530

    Made a few changes in the GUI file\n More to follow

--- GUI/GUI_Servo_control.py
+++ GUI/GUI_Servo_control.py
@@ -1,8 +1,9 @@
 from tkinter import *
 
 def show_values():
-    print (w1.get(), w2.get(), w3.get(), w4.get())
-
+    print (w1.get(), w2.get(), w3.get())
+def grab():
+	print("Grabbed the object")
 master = Tk()
 master.title("Servo Angle Control")
 w1 = Scale(master, from_=0, to=180,length = 600,tickinterval=10, orient=HORIZONTAL)
@@ -14,11 +15,11 @@ w2.pack()
 w3 = Scale(master, from_=0, to=180,length = 600,tickinterval=10, orient=HORIZONTAL)
 w3.set(0)
 w3.pack()
-w4 = Scale(master, from_=0, to=180,length = 600,tickinterval=10, orient=HORIZONTAL)
-w4.set(0)
-w4.pack()
-
-
+label1 = Label(master,text="")
+label1.pack()
 Button(master, text='Show', command=show_values).pack()
+label2 = Label(master,text="")
+label2.pack()
+Button(master, text='GRAB', command=grab).pack()
 
 mainloop()
