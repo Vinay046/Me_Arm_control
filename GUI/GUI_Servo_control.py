5a8d21e9f756999c985a8669b06de0a50e7318ee
Vinay Patil <vinayvp.98@gmail.com>
2019-07-08 16:11:20 +0530

    Made changes in the GUI file

--- GUI/GUI_Servo_control.py
+++ GUI/GUI_Servo_control.py
@@ -1,30 +1,42 @@
 import tkinter as tk
-def show_values():
-    print (w1.get(), w2.get(), w3.get())
-def grab():
-	print("Grabbed the object")
+import itertools
+import serial
+
+#ser = serial.Serial('COM3',9600)
+global button_state 
+
+def toggle(icycle=itertools.cycle(['Release','Grab'])):
+    state = next(icycle)
+    button1['text'] = str(state)
+    message = "@"+str(w1.get())+","+str(w2.get())+","+str(w3.get())+","+button1['text']+"!"
+    #ser.write(message.encode())
+    print(message)
+
+def show_values(self):
+    message = "@"+str(w1.get())+","+str(w2.get())+","+str(w3.get())+button1['text']+"!"
+    #ser.write(message.encode())
+    print(message)
 
 master = tk.Tk()
 
-master.geometry("700x300")
+master.geometry("700x250")
 master.resizable(0,0)
+
 master.title("Servo Angle Control")
-w1 = tk.Scale(master, from_=0, to=180,length = 600,tickinterval=180, orient=tk.HORIZONTAL)
+
+w1 = tk.Scale(master, from_=0, to=180,length = 600,tickinterval=90, orient=tk.HORIZONTAL,command = show_values)
 w1.set(0)
 w1.pack()
-w2 = tk.Scale(master, from_=0, to=180,length = 600,tickinterval=180, orient=tk.HORIZONTAL)
+
+w2 = tk.Scale(master, from_=0, to=180,length = 600,tickinterval=90, orient=tk.HORIZONTAL,command = show_values)
 w2.set(0)
 w2.pack()
-w3 = tk.Scale(master, from_=0, to=180,length = 600,tickinterval=180, orient=tk.HORIZONTAL)
+
+w3 = tk.Scale(master, from_=0, to=180,length = 600,tickinterval=90, orient=tk.HORIZONTAL,command = show_values)
 w3.set(0)
 w3.pack()
-label1 = tk.Label(master,text="")
-label1.pack()
-tk.Button(master, text='Show', command=show_values).pack()
-label2 = tk.Label(master,text="")
-label2.pack()
-tk.Button(master, text='GRAB', command=grab).pack()
-label3 = tk.Label(master,text="")
-label3.pack()
+
+button1 = tk.Button(master,text = 'Grab', command=toggle)
+button1.pack()
 
 master.mainloop()
